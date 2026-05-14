aeabfaec42e1b86723c449d45c0035f0d5d51f1e
Pipo2410 <pipo2410@gmail.com>
2026-05-14 08:48:52 +0100

    chore(app): setup husky for pre-commit checks, conventional commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 const App = () => {
   return (
     <div>
-      <h1 className='text-2'>Hello to portfolio website</h1>
+      <h1 className="text-2">Hello to portfolio website</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
