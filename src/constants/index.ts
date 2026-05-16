4e3f05720b8ae7d0f91ceefb2d68ac528a439cae
Pipo2410 <pipo2410@gmail.com>
2026-05-16 19:12:12 +0100

    feat(navbar): Add navbar

---
+++ src/constants/index.ts
@@ -0,0 +1,486 @@
+const navLinks = [
+  {
+    id: 1,
+    name: 'Projects',
+    type: 'finder',
+  },
+  {
+    id: 3,
+    name: 'Contact',
+    type: 'contact',
+  },
+  {
+    id: 4,
+    name: 'Resume',
+    type: 'resume',
+  },
+]
+
+const navIcons = [
+  {
+    id: 1,
+    img: '/icons/wifi.svg',
+  },
+  {
+    id: 2,
+    img: '/icons/search.svg',
+  },
+  {
+    id: 3,
+    img: '/icons/user.svg',
+  },
+  {
+    id: 4,
+    img: '/icons/mode.svg',
+  },
+]
+
+const dockApps = [
+  {
+    id: 'finder',
+    name: 'Portfolio', // was "Finder"
+    icon: 'finder.png',
+    canOpen: true,
+  },
+  {
+    id: 'safari',
+    name: 'Articles', // was "Safari"
+    icon: 'safari.png',
+    canOpen: true,
+  },
+  {
+    id: 'photos',
+    name: 'Gallery', // was "Photos"
+    icon: 'photos.png',
+    canOpen: true,
+  },
+  {
+    id: 'contact',
+    name: 'Contact', // or "Get in touch"
+    icon: 'contact.png',
+    canOpen: true,
+  },
+  {
+    id: 'terminal',
+    name: 'Skills', // was "Terminal"
+    icon: 'terminal.png',
+    canOpen: true,
+  },
+  {
+    id: 'trash',
+    name: 'Archive', // was "Trash"
+    icon: 'trash.png',
+    canOpen: false,
+  },
+]
+
+const blogPosts = [
+  {
+    id: 1,
+    date: 'Sep 2, 2026',
+    title: 'Blog1',
+    image: '/images/blog1.png',
+    link: 'https://google.com',
+  },
+  {
+    id: 2,
+    date: 'Aug 28, 2026',
+    title: 'Blog2',
+    image: '/images/blog2.png',
+    link: 'https://google.com',
+  },
+  {
+    id: 3,
+    date: 'Aug 15, 2026',
+    title: 'Blog3',
+    image: '/images/blog3.png',
+    link: 'https://google.com',
+  },
+]
+
+const techStack = [
+  {
+    category: 'Frontend',
+    items: ['React.js', 'Next.js', 'TypeScript'],
+  },
+  {
+    category: 'Styling',
+    items: ['Tailwind CSS', 'Sass', 'CSS'],
+  },
+  {
+    category: 'Backend',
+    items: ['Node.js', 'Express', 'NestJS', 'Hono'],
+  },
+  {
+    category: 'Database',
+    items: ['MongoDB', 'PostgreSQL'],
+  },
+  {
+    category: 'Dev Tools',
+    items: ['Git', 'GitHub', 'Docker'],
+  },
+]
+
+const socials = [
+  {
+    id: 1,
+    text: 'Github',
+    icon: '/icons/github.svg',
+    bg: '#f4656b',
+    link: 'https://github.com/Pipo2410',
+  },
+  {
+    id: 2,
+    text: 'Platform',
+    icon: '/icons/atom.svg',
+    bg: '#4bcb63',
+    link: 'https://peter.com',
+  },
+  {
+    id: 3,
+    text: 'Twitter/X',
+    icon: '/icons/twitter.svg',
+    bg: '#ff866b',
+    link: 'https://x.com/Pipo2410',
+  },
+  {
+    id: 4,
+    text: 'LinkedIn',
+    icon: '/icons/linkedin.svg',
+    bg: '#05b6f6',
+    link: 'https://www.linkedin.com/in/peter-strelec',
+  },
+]
+
+const photosLinks = [
+  {
+    id: 1,
+    icon: '/icons/gicon1.svg',
+    title: 'Library',
+  },
+  {
+    id: 2,
+    icon: '/icons/gicon2.svg',
+    title: 'Memories',
+  },
+  {
+    id: 3,
+    icon: '/icons/file.svg',
+    title: 'Places',
+  },
+  {
+    id: 4,
+    icon: '/icons/gicon4.svg',
+    title: 'People',
+  },
+  {
+    id: 5,
+    icon: '/icons/gicon5.svg',
+    title: 'Favorites',
+  },
+]
+
+const gallery = [
+  {
+    id: 1,
+    img: '/images/gal1.png',
+  },
+  {
+    id: 2,
+    img: '/images/gal2.png',
+  },
+  {
+    id: 3,
+    img: '/images/gal3.png',
+  },
+  {
+    id: 4,
+    img: '/images/gal4.png',
+  },
+]
+
+export {
+  navLinks,
+  navIcons,
+  dockApps,
+  blogPosts,
+  techStack,
+  socials,
+  photosLinks,
+  gallery,
+}
+
+const WORK_LOCATION = {
+  id: 1,
+  type: 'work',
+  name: 'Work',
+  icon: '/icons/work.svg',
+  kind: 'folder',
+  children: [
+    // ▶ Project 1
+    {
+      id: 5,
+      name: 'AI Support platform',
+      icon: '/images/folder.png',
+      kind: 'folder',
+      position: 'top-10 left-5', // icon position inside Finder
+      windowPosition: 'top-[5vh] left-5', // optional: Finder window position
+      children: [
+        {
+          id: 1,
+          name: 'Nike Project.txt',
+          icon: '/images/txt.png',
+          kind: 'file',
+          fileType: 'txt',
+          position: 'top-5 left-10',
+          description: [
+            'The AI support platform allows users to have communicate with users with help of AI, as well as manually interfere when needed.',
+          ],
+        },
+        {
+          id: 2,
+          name: 'AI support platform Web app',
+          icon: '/images/safari.png',
+          kind: 'file',
+          fileType: 'url',
+          href: 'https://ai-support-platform-web.vercel.app',
+          position: 'top-10 right-20',
+        },
+        {
+          id: 4,
+          name: 'ai-support.png',
+          icon: '/images/image.png',
+          kind: 'file',
+          fileType: 'img',
+          position: 'top-52 right-80',
+          imageUrl: '/images/project-1.png',
+        },
+        {
+          id: 5,
+          name: 'Design.fig',
+          icon: '/images/plain.png',
+          kind: 'file',
+          fileType: 'fig',
+          href: 'https://google.com',
+          position: 'top-60 right-20',
+        },
+      ],
+    },
+
+    // ▶ Project 2
+    {
+      id: 6,
+      name: 'Loopline',
+      icon: '/images/folder.png',
+      kind: 'folder',
+      position: 'top-52 right-80',
+      windowPosition: 'top-[20vh] left-7',
+      children: [
+        {
+          id: 1,
+          name: 'AI Resume Analyzer Project.txt',
+          icon: '/images/txt.png',
+          kind: 'file',
+          fileType: 'txt',
+          position: 'top-5 right-10',
+          description: ['Slack clone with AI'],
+        },
+        {
+          id: 2,
+          name: 'loopline-web.vercel.app',
+          icon: '/images/safari.png',
+          kind: 'file',
+          fileType: 'url',
+          href: 'https://loopline-web.vercel.app',
+          position: 'top-20 left-20',
+        },
+        {
+          id: 4,
+          name: 'ai-loopline.png',
+          icon: '/images/image.png',
+          kind: 'file',
+          fileType: 'img',
+          position: 'top-52 left-80',
+          imageUrl: '/images/project-2.png',
+        },
+        {
+          id: 5,
+          name: 'Design.fig',
+          icon: '/images/plain.png',
+          kind: 'file',
+          fileType: 'fig',
+          href: 'https://google.com',
+          position: 'top-60 left-5',
+        },
+      ],
+    },
+
+    // ▶ Project 3
+    {
+      id: 7,
+      name: 'Some other project',
+      icon: '/images/folder.png',
+      kind: 'folder',
+      position: 'top-10 left-80',
+      windowPosition: 'top-[33vh] left-7',
+      children: [
+        {
+          id: 1,
+          name: 'Some other project.txt',
+          icon: '/images/txt.png',
+          kind: 'file',
+          fileType: 'txt',
+          position: 'top-5 left-10',
+          description: ['lorem ipsum dolor sit amet'],
+        },
+        {
+          id: 2,
+          name: 'food-delivery-app.com',
+          icon: '/images/safari.png',
+          kind: 'file',
+          fileType: 'url',
+          href: 'https://google.com',
+          position: 'top-10 right-20',
+        },
+        {
+          id: 4,
+          name: 'sth-else.png',
+          icon: '/images/image.png',
+          kind: 'file',
+          fileType: 'img',
+          position: 'top-52 right-80',
+          imageUrl: '/images/project-3.png',
+        },
+        {
+          id: 5,
+          name: 'Design.fig',
+          icon: '/images/plain.png',
+          kind: 'file',
+          fileType: 'fig',
+          href: 'https://google.com',
+          position: 'top-60 right-20',
+        },
+      ],
+    },
+  ],
+}
+
+const ABOUT_LOCATION = {
+  id: 2,
+  type: 'about',
+  name: 'About me',
+  icon: '/icons/info.svg',
+  kind: 'folder',
+  children: [
+    {
+      id: 1,
+      name: 'me.png',
+      icon: '/images/image.png',
+      kind: 'file',
+      fileType: 'img',
+      position: 'top-10 left-5',
+      imageUrl: '/images/peter.jpg',
+    },
+    {
+      id: 2,
+      name: 'casual-me.png',
+      icon: '/images/image.png',
+      kind: 'file',
+      fileType: 'img',
+      position: 'top-28 right-72',
+      imageUrl: '/images/peter-2.jpg',
+    },
+    {
+      id: 3,
+      name: 'conference-me.png',
+      icon: '/images/image.png',
+      kind: 'file',
+      fileType: 'img',
+      position: 'top-52 left-80',
+      imageUrl: '/images/peter-3.jpeg',
+    },
+    {
+      id: 4,
+      name: 'about-me.txt',
+      icon: '/images/txt.png',
+      kind: 'file',
+      fileType: 'txt',
+      position: 'top-60 left-5',
+      subtitle: 'Meet the Developer Behind the Code',
+      image: '/images/peter.jpg',
+      description: [
+        'Hey! I’m Peter 👋, a web developer who enjoys building sleek, interactive websites that actually work well.',
+        'I specialize in JavaScript, React, and Next.js—and I love making things feel smooth, fast, and just a little bit delightful.',
+      ],
+    },
+  ],
+}
+
+const RESUME_LOCATION = {
+  id: 3,
+  type: 'resume',
+  name: 'Resume',
+  icon: '/icons/file.svg',
+  kind: 'folder',
+  children: [
+    {
+      id: 1,
+      name: 'Resume.pdf',
+      icon: '/images/pdf.png',
+      kind: 'file',
+      fileType: 'pdf',
+      // you can add `href` if you want to open a hosted resume
+      // href: "/your/resume/path.pdf",
+    },
+  ],
+}
+
+const TRASH_LOCATION = {
+  id: 4,
+  type: 'trash',
+  name: 'Trash',
+  icon: '/icons/trash.svg',
+  kind: 'folder',
+  children: [
+    {
+      id: 1,
+      name: 'trash1.png',
+      icon: '/images/image.png',
+      kind: 'file',
+      fileType: 'img',
+      position: 'top-10 left-10',
+      imageUrl: '/images/trash-1.png',
+    },
+    {
+      id: 2,
+      name: 'trash2.png',
+      icon: '/images/image.png',
+      kind: 'file',
+      fileType: 'img',
+      position: 'top-40 left-80',
+      imageUrl: '/images/trash-2.png',
+    },
+  ],
+}
+
+export const locations = {
+  work: WORK_LOCATION,
+  about: ABOUT_LOCATION,
+  resume: RESUME_LOCATION,
+  trash: TRASH_LOCATION,
+}
+
+const INITIAL_Z_INDEX = 1000
+
+const WINDOW_CONFIG = {
+  finder: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  contact: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  resume: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  safari: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  photos: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  terminal: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  txtfile: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  imgfile: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+}
+
+export { INITIAL_Z_INDEX, WINDOW_CONFIG }
